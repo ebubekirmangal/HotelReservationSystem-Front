9b2c767457b25fff4b0eb0fbb0644921d22b6f2a
Beste <40505110+besteda@users.noreply.github.com>
2024-06-20 22:03:02 +0300

    responsive design added.

--- src/app/layout/navigation/navigation.component.ts
+++ src/app/layout/navigation/navigation.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, Input, OnChanges, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
@@ -16,28 +16,32 @@ import { TranslateModule, TranslateService } from '@ngx-translate/core';
     FormsModule
   ],
   templateUrl: './navigation.component.html',
-  styleUrl: './navigation.component.css',
+  styleUrls: ['./navigation.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class NavigationComponent implements OnChanges{
-selectedLanguage: string = "English";
+export class NavigationComponent implements OnInit {
+  selectedLanguage: string = "en";
 
-constructor(private translate: TranslateService) {}
+  constructor(private translate: TranslateService) {}
 
   ngOnInit(): void {
     const defaultLanguage = localStorage.getItem('language') || 'en';
     this.selectedLanguage = defaultLanguage; 
     this.translate.setDefaultLang(defaultLanguage); 
     this.translate.use(defaultLanguage);
-    this.changeLanguage(this.selectedLanguage);
-  }
-
-  ngOnChanges(): void {
-    this.changeLanguage(this.selectedLanguage);
   }
 
   changeLanguage(lang: string): void {
     this.translate.use(lang); 
     localStorage.setItem('language', lang); 
-    this.selectedLanguage = lang;}
- }
+    this.selectedLanguage = lang;
+  }
+
+  toggleDropdownMenu(): void {
+    const dropdownMenu = document.querySelector('.dropdown-menu') as HTMLElement;
+    const toggleBtnIcon = document.querySelector('.toggle-btn i') as HTMLElement;
+    dropdownMenu.classList.toggle('open');
+    const isOpen = dropdownMenu.classList.contains('open');
+    toggleBtnIcon.className = isOpen ? 'fa-solid fa-xmark' : 'fa-solid fa-bars';
+  }   
+}
