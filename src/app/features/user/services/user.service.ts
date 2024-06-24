0712f71f839b7dcc180d9e037fda1530ca29ac30
ebubekirmangal <mangalbekir1880@gmail.com>
2024-06-24 16:16:23 +0300

    user service eklendi, angular jwt eklendi

--- src/app/features/user/services/user.service.ts
+++ src/app/features/user/services/user.service.ts
@@ -1,10 +1,36 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-apiControlUrl = "http://localhost:8080"
-  constructor() { }
+apiControlUrl = "http://localhost:8080/api/v1/auth";
+private jwtHelper = new JwtHelperService();
+  constructor(private http: HttpClient, private router: Router) { }
+
+  login(credentials) {
+    return this.http.post(`${this.apiControlUrl}/login`, credentials)
+      .subscribe((response: any) => {
+        localStorage.setItem('token', response.token);
+        this.router.navigate(['/home']);
+      });
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    this.router.navigate(['/login']);
+  }
+
+  isLoggedIn() {
+    const token = localStorage.getItem('token');
+    return token && !this.jwtHelper.isTokenExpired(token);
+  }
+
+  register(){
+    
+  }
 
 }
